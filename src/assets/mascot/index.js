159d7ebe12f837699590c03b565b74ab27041486
seoyooni <choiseoyooni1016@naver.com>
2026-09-10 02:32:27 +0900

    mascot images

---
+++ src/assets/mascot/index.js
@@ -0,0 +1,19 @@
+import alert from './alert.png'
+import analyzing from './analyzing.png'
+import connecting from './connecting.png'
+import disconnected from './disconnected.png'
+import empty from './empty.png'
+import normal from './normal.png'
+import paused from './paused.png'
+import success from './success.png'
+
+export const mascotByState = {
+  alert,
+  analyzing,
+  connecting,
+  disconnected,
+  empty,
+  normal,
+  paused,
+  success,
+}
