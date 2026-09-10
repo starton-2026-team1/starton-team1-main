42162509ce3ab8303b991f9b8b5e0db2380da71d
seoyooni <choiseoyooni1016@naver.com>
2026-09-10 15:32:46 +0900

    signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import AuthLandingPage from './features/auth/pages/AuthLandingPage'
 import LoginPage from './features/auth/pages/LoginPage'
+import SignUpPage from './features/auth/pages/SignUpPage'
 
 function App() {
   const [page, setPage] = useState('landing')
@@ -9,9 +10,14 @@ function App() {
     return <LoginPage onBack={() => setPage('landing')} />
   }
 
+  if (page === 'signup') {
+    return <SignUpPage onBack={() => setPage('landing')} />
+  }
+
   return (
     <AuthLandingPage
       onEmailLogin={() => setPage('login')}
+      onSignUp={() => setPage('signup')}
     />
   )
 }
